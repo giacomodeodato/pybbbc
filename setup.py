f0fb2cebfd6f462f6b1c0ba23eeca197dc611ea0
Zach Barry <zbarry@protonmail.com>
2021-07-21 19:13:50 -0400

    add setup.py and add standard github python .gitignore contents

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pybbbc',
+    version='0.0',
+    url='https://github.com/giacomodeodato/pybbbc.git',
+    author='Giacomo Deodato',
+    author_email='',
+    description='This is a python interface to the BBBC021 dataset of cellular images (Caie et al., Molecular Cancer Therapeutics, 2010), available from the Broad Bioimage Benchmark Collection (Ljosa et al., Nature Methods, 2012).',
+    packages=find_packages(),    
+    install_requires=['h5py', 'ipywidgets'],
+)
